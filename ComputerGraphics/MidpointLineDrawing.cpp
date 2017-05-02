a55a426379d495083ca689fdcec626e17d26874c
Shubham Mathur <shubham.mathur2014@vit.ac.in>
2017-05-02 12:45:02 +0530

    Update MidpointLineDrawing.cpp

--- ComputerGraphics/MidpointLineDrawing.cpp
+++ ComputerGraphics/MidpointLineDrawing.cpp
@@ -1,40 +1,89 @@
-#include "stdafx.h"
 #include<iostream>
-#include<cstdlib>
-#include "graphics.h"
+#include"graphics.h"
 using namespace std;
-void midpoint(int x1, int y1, int x2, int y2);
+void mid(int x1, int y1, int x2, int y2);
+void floodfill(int x, int y, int color, int backcolor)
+{
+	int region = getpixel(x, y);
+	if (region == backcolor)
+	{
+		putpixel(x, y, color);
+		floodfill(x + 1, y, color, backcolor);
+		floodfill(x - 1, y, color, backcolor);
+		floodfill(x, y + 1, color, backcolor);
+		floodfill(x, y - 1, color, backcolor);
+	}
+}
 int main()
 {
-	initwindow(400, 400);
+	initwindow(800, 800);
 	int x1, y1, x2, y2;
-	cout << "Enter initial point:";
-	cin >> x1 >> y1;
-	cout << "Enter final point:";
-	cin >> x2 >> y2;
-	midpoint(x1, y1, x2, y2);
+	mid(500, 320, 600, 220);//left
+	mid(600, 220, 700, 320);//right
+	mid(500, 320, 700, 320);//bottom
+	//floodfill(550, 310, 10, 0);
 	while (!kbhit());
 	closegraph();
 	return 0;
 }
-void midpoint(int x1, int y1, int x2, int y2)
+void mid(int x1, int y1, int x2, int y2)
 {
-	int dx = x2 - x1;
-	int dy = y2 - y1;
-	putpixel(x1, y1, 10);
-	int f = dy- dx/2;
-	for (int k = 0, x = x1, y = y1; k<dx; k++, x++)
+	double dx = x2 - x1, dy = y2 - y1;
+	putpixel(x1, y1, 3);
+	putpixel(x2, y2, 3);
+	if (abs(dy / dx) < 1)
 	{
-		if (f<0)
-		{
-			putpixel(x + 1, y, 10);
-			f = f + dy;
-		}
+		double d =  dy - dx/2;
+		int x = x1>x2 ? x2 : x1;
+		int y = x1>x2 ? y2 : y1;
+		int inc;
+		if ((dy / dx) < 0)
+			inc = -1;
 		else
+			inc = 1;
+		dx = abs(dx);
+		dy = abs(dy);
+		for (int i = 0; i <= dx; i++, x++)
 		{
-			putpixel(x + 1, y + 1, 10);
-			y++;
-			f = f + dy-dx;
+
+			if (d < 0)
+			{
+				putpixel(x + 1, y, 3);
+				d = d + dy;
+			}
+			else
+			{
+				putpixel(x + 1, y + inc, 3);
+				y += inc;
+				d = d +  (dy - dx);
+			}
+		}
+	}
+	else
+	{
+		double d =  dx - dy/2;
+		int x = y1>y2 ? x2 : x1;
+		int y = y1>y2 ? y2 : y1;
+		int inc;
+		if ((dy / dx) < 0)
+			inc = -1;
+		else
+			inc = 1;
+		dx = abs(dx);
+		dy = abs(dy);
+		for (int i = 0; i <= dy; i++, y++)
+		{
+			if (d < 0)
+			{
+				putpixel(x, y + 1, 3);
+				d = d + dx;
+			}
+			else
+			{
+				putpixel(x + inc, y + 1, 3);
+				x += inc;
+				d = d +  (dx - dy);
+			}
 		}
 	}
 }
